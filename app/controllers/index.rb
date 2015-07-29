1d86c60a036e1044127533a2a64c6f1d9bb40d43
agweiler <kheryee@live.com>
2015-07-29 13:37:54 +0800

    complete

---
+++ app/controllers/index.rb
@@ -0,0 +1,25 @@
+
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_cool_url
+end
+
+get '/post_something' do
+  puts "[LOG] Getting /post_something"
+  puts "[LOG] Params: #{params.inspect}"
+  @apple
+  erb :post_something
+end
